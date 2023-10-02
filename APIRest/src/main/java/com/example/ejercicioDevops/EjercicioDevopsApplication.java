c3d40f97a3831b680b00b52a889a16e66d293583
sergiobs <62768751+sergiobs01@users.noreply.github.com>
2023-10-02 14:42:45 +0200

    Apirest

---
+++ APIRest/src/main/java/com/example/ejercicioDevops/EjercicioDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ejercicioDevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioDevopsApplication.class, args);
+	}
+
+}
